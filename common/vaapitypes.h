e13bf4dcaa93c68f2e028f32c10276fd217c53f1
XuGuangxin <guangxin.xu@intel.com>
2014-05-07 10:37:44 +0800

    vaapipicture: move picture type to vaapitype since encoder need it

--- common/vaapitypes.h
+++ common/vaapitypes.h
@@ -91,4 +91,16 @@ typedef enum {
       className(const className&); \
       className & operator=(const className&); \
 
+
+typedef enum {
+    VAAPI_PICTURE_TYPE_NONE = 0,        // Undefined
+    VAAPI_PICTURE_TYPE_I,               // Intra
+    VAAPI_PICTURE_TYPE_P,               // Predicted
+    VAAPI_PICTURE_TYPE_B,               // Bi-directional predicted
+    VAAPI_PICTURE_TYPE_S,               // S(GMC)-VOP (MPEG-4)
+    VAAPI_PICTURE_TYPE_SI,              // Switching Intra
+    VAAPI_PICTURE_TYPE_SP,              // Switching Predicted
+    VAAPI_PICTURE_TYPE_BI,              // BI type (VC-1)
+} VaapiPictureType;
+
 #endif                          /* vaapitypes_h */
