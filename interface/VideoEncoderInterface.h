9eef153bc68bdc51e68d49013b83f27658b424b2
XuGuangxin <Guangxin.Xu@intel.com>
2014-06-12 13:52:05 +0800

    doc: doxygen support for VideoEncoderInterface.h

--- interface/VideoEncoderInterface.h
+++ interface/VideoEncoderInterface.h
@@ -1,5 +1,5 @@
 /*
- *  VideoEncoderInterface.h- basic va decoder for video
+ *  VideoEncoderInterface.h- basic va encoder for video
  *
  *  Copyright (C) 2013 Intel Corporation
  *
@@ -24,25 +24,44 @@
 
 #include "VideoEncoderDef.h"
 
+/**
+ * \class IVideoEncoder
+ * \brief Abstract video encoding interface of libyami
+ * it is the interface with client
+ */
 class IVideoEncoder {
   public:
-    virtual ~ IVideoEncoder() {
-    };
+    virtual ~ IVideoEncoder() {}
+    /** \brief start encode, make sure you setParameters before start, else if will use default.
+    */
     virtual Encode_Status start(void) = 0;
+    /// stop encoding and destroy encoder context.
     virtual Encode_Status stop(void) = 0;
-    virtual void flush(void) = 0;
+
+    /// continue encoding with new data in @param[in] inBuffer
     virtual Encode_Status encode(VideoEncRawBuffer * inBuffer) = 0;
+    /**
+     * \brief return one frame encoded data to client;
+     * ENCODE_BUFFER_NO_MORE will be returned if no available frame
+     */
     virtual Encode_Status getOutput(VideoEncOutputBuffer * outBuffer) = 0;
-    virtual Encode_Status getParameters(VideoParamConfigSet *
-                                        videoEncParams) = 0;
-    virtual Encode_Status setParameters(VideoParamConfigSet *
-                                        videoEncParams) = 0;
-    virtual Encode_Status getConfig(VideoParamConfigSet * videoEncConfig) =
-        0;
-    virtual Encode_Status setConfig(VideoParamConfigSet * videoEncConfig) =
-        0;
+    /// get encoder params
+    virtual Encode_Status getParameters(VideoParamConfigSet * videoEncParams) = 0;
+    /// set encoder params
+    virtual Encode_Status setParameters(VideoParamConfigSet * videoEncParams) = 0;
+    /// get max coded buffer size.
     virtual Encode_Status getMaxOutSize(uint32_t * maxSize) = 0;
+
+    /// get encode statistics information, for debug use
     virtual Encode_Status getStatistics(VideoStatistics * videoStat) = 0;
+
+    ///obsolete, discard cached data (input data or encoded video frames), not sure why an encoder need this
+    virtual void flush(void) = 0;
+    ///obsolete, what is the difference between  getParameters and getConfig?
+    virtual Encode_Status getConfig(VideoParamConfigSet * videoEncConfig) = 0;
+    ///obsolete, what is the difference between  setParameters and setConfig?
+    virtual Encode_Status setConfig(VideoParamConfigSet * videoEncConfig) = 0;
+
 };
 
 #endif                          /* VIDEO_ENCODER_INTERFACE_H_ */
