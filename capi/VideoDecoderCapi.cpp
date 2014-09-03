2bd65284c2c84fe3a9fffbd7302bc7b2c9060353
Tang Xin <xin.t.tang@intel.com>
2014-09-03 09:48:41 +0800

    Fix code style in capi.
    Signed-off-by: Tang Xin <xin.t.tang@intel.com>

--- capi/VideoDecoderCapi.cpp
+++ capi/VideoDecoderCapi.cpp
@@ -11,89 +11,111 @@ DecodeHandler createDecoder(const char *mimeType)
 
 Decode_Status decodeStart(DecodeHandler p, VideoConfigBuffer *buffer)
 {
-    if(p) return ((IVideoDecoder*)p)->start(buffer);
-    else return DECODE_FAIL;
+    if(p)
+        return ((IVideoDecoder*)p)->start(buffer);
+    else
+        return DECODE_FAIL;
 }
 
 Decode_Status decodeReset(DecodeHandler p, VideoConfigBuffer *buffer)
 {
-     if(p) return ((IVideoDecoder*)p)->reset(buffer);
-     else return DECODE_FAIL;
+     if(p)
+        return ((IVideoDecoder*)p)->reset(buffer);
+     else
+        return DECODE_FAIL;
 }
 
 void decodeStop(DecodeHandler p)
 {
-     if(p) ((IVideoDecoder*)p)->stop();
+     if(p)
+        ((IVideoDecoder*)p)->stop();
 }
 
 void decodeFlush(DecodeHandler p)
 {
-     if(p) ((IVideoDecoder*)p)->flush();
+     if(p)
+        ((IVideoDecoder*)p)->flush();
 }
 
 Decode_Status decode(DecodeHandler p, VideoDecodeBuffer *buffer)
 {
-     if(p) return ((IVideoDecoder*)p)->decode(buffer);
-     else return DECODE_FAIL;
+     if(p)
+        return ((IVideoDecoder*)p)->decode(buffer);
+     else
+        return DECODE_FAIL;
 }
 
 const VideoRenderBuffer* decodeGetOutput(DecodeHandler p, bool draining)
 {
-     if(p) return ((IVideoDecoder*)p)->getOutput(draining);
+     if(p)
+        return ((IVideoDecoder*)p)->getOutput(draining);
 }
 
 Decode_Status decodeGetOutput_x11(DecodeHandler p, Drawable draw, int64_t *timeStamp
         , int drawX, int drawY, int drawWidth, int drawHeight, bool draining
         , int frameX, int frameY, int frameWidth, int frameHeight)
 {
-    if(p) return ((IVideoDecoder*)p)->getOutput(draw, timeStamp, drawX, drawY, drawWidth, drawHeight, draining
+    if(p)
+        return ((IVideoDecoder*)p)->getOutput(draw, timeStamp, drawX, drawY, drawWidth, drawHeight, draining
                         , frameX, frameY, frameWidth, frameHeight);
-    else return DECODE_FAIL;
+    else
+        return DECODE_FAIL;
 }
 
 const VideoFormatInfo* getFormatInfo(DecodeHandler p)
 {
-    if(p) return ((IVideoDecoder*)p)->getFormatInfo();
+    if(p)
+        return ((IVideoDecoder*)p)->getFormatInfo();
 }
 
 void renderDone(DecodeHandler p, VideoRenderBuffer* buffer)
 {
-    if(p) ((IVideoDecoder*)p)->renderDone(buffer);
+    if(p)
+        ((IVideoDecoder*)p)->renderDone(buffer);
 }
 
 void decodeSetNativeDisplay(DecodeHandler p, NativeDisplay * display)
 {
-    if(p) ((IVideoDecoder*)p)->setNativeDisplay(display);
+    if(p)
+        ((IVideoDecoder*)p)->setNativeDisplay(display);
 }
 
 void flushOutport(DecodeHandler p)
 {
-    if(p) ((IVideoDecoder*)p)->flushOutport();
+    if(p)
+        ((IVideoDecoder*)p)->flushOutport();
 }
 
 void enableNativeBuffers(DecodeHandler p)
 {
-    if(p) ((IVideoDecoder*)p)->enableNativeBuffers();
+    if(p)
+        ((IVideoDecoder*)p)->enableNativeBuffers();
 }
 
 Decode_Status getClientNativeWindowBuffer(DecodeHandler p, void *bufferHeader, void *nativeBufferHandle)
 {
-    if(p) return ((IVideoDecoder*)p)->getClientNativeWindowBuffer(bufferHeader, nativeBufferHandle);
-    else return DECODE_FAIL;
+    if(p)
+        return ((IVideoDecoder*)p)->getClientNativeWindowBuffer(bufferHeader, nativeBufferHandle);
+    else
+        return DECODE_FAIL;
 }
 
 Decode_Status flagNativeBuffer(DecodeHandler p, void * pBuffer)
 {
-    if(p) return ((IVideoDecoder*)p)->flagNativeBuffer(pBuffer);
-    else return DECODE_FAIL;
+    if(p)
+        return ((IVideoDecoder*)p)->flagNativeBuffer(pBuffer);
+    else
+        return DECODE_FAIL;
 }
 
 void releaseLock(DecodeHandler p)
 {
-    if(p) ((IVideoDecoder*)p)->releaseLock();
+    if(p)
+        ((IVideoDecoder*)p)->releaseLock();
 }
 
 void releaseDecoder(DecodeHandler p)
 {
-    if(p) releaseVideoDecoder((IVideoDecoder*)p);
+    if(p)
+        releaseVideoDecoder((IVideoDecoder*)p);
 }
--- capi/VideoEncoderCapi.cpp
+++ capi/VideoEncoderCapi.cpp
@@ -11,69 +11,90 @@ EncodeHandler createEncoder(const char *mimeType)
 
 void encodeSetNativeDisplay(EncodeHandler p, NativeDisplay * display)
 {
-    if(p) ((IVideoEncoder*)p)->setNativeDisplay(display);
+    if(p)
+        ((IVideoEncoder*)p)->setNativeDisplay(display);
 }
 
 Encode_Status encodeStart(EncodeHandler p)
 {
-    if(p) return ((IVideoEncoder*)p)->start();
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->start();
+    else
+        return ENCODE_FAIL;
 }
 
 void encodeStop(EncodeHandler p)
 {
-    if(p) ((IVideoEncoder*)p)->stop();
+    if(p)
+        ((IVideoEncoder*)p)->stop();
 }
 
 Encode_Status encode(EncodeHandler p, VideoEncRawBuffer * inBuffer)
 {
-    if(p) return ((IVideoEncoder*)p)->encode(inBuffer);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->encode(inBuffer);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status encodeGetOutput(EncodeHandler p, VideoEncOutputBuffer * outBuffer, bool withWait)
 {
-    if(p) return ((IVideoEncoder*)p)->getOutput(outBuffer, withWait);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->getOutput(outBuffer, withWait);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status getParameters(EncodeHandler p, VideoParamConfigType type, Yami_PTR videoEncParams)
 {
-    if(p) return ((IVideoEncoder*)p)->getParameters(type, videoEncParams);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->getParameters(type, videoEncParams);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status setParameters(EncodeHandler p, VideoParamConfigType type, Yami_PTR videoEncParams)
 {
-    if(p) return ((IVideoEncoder*)p)->setParameters(type, videoEncParams);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->setParameters(type, videoEncParams);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status getMaxOutSize(EncodeHandler p, uint32_t * maxSize)
 {
-    if(p) return ((IVideoEncoder*)p)->getMaxOutSize(maxSize);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->getMaxOutSize(maxSize);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status getStatistics(EncodeHandler p, VideoStatistics * videoStat)
 {
-    if(p) return ((IVideoEncoder*)p)->getStatistics(videoStat);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->getStatistics(videoStat);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status getConfig(EncodeHandler p, VideoParamConfigType type, Yami_PTR videoEncConfig)
 {
-    if(p) return ((IVideoEncoder*)p)->getConfig(type, videoEncConfig);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->getConfig(type, videoEncConfig);
+    else
+        return ENCODE_FAIL;
 }
 
 Encode_Status setConfig(EncodeHandler p, VideoParamConfigType type, Yami_PTR videoEncConfig)
 {
-    if(p) return ((IVideoEncoder*)p)->setConfig(type, videoEncConfig);
-    else return ENCODE_FAIL;
+    if(p)
+        return ((IVideoEncoder*)p)->setConfig(type, videoEncConfig);
+    else
+        return ENCODE_FAIL;
 }
 
 void releaseEncoder(EncodeHandler p)
 {
-    if(p) releaseVideoEncoder((IVideoEncoder*)p);
+    if(p)
+        releaseVideoEncoder((IVideoEncoder*)p);
 }
