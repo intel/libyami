d724574a24226378c18f9e5dcaa14d9c49b41328
XuGuangxin <Guangxin.Xu@intel.com>
2014-11-13 16:14:04 +0800

    h264dec: work around for crop

--- decoder/vaapidecoder_h264.h
+++ decoder/vaapidecoder_h264.h
@@ -1,5 +1,5 @@
 /*
- *  vaapidecoder_h264.h 
+ *  vaapidecoder_h264.h
  *
  *  Copyright (C) 2011-2014 Intel Corporation
  *    Author: Gwenole Beauchesne <gwenole.beauchesne@intel.com>
@@ -369,6 +369,9 @@ class VaapiDecoderH264:public VaapiDecoderBase {
     bool m_isAVC;
     bool m_resetContext;
     DISALLOW_COPY_AND_ASSIGN(VaapiDecoderH264);
+    //temporary work around, remove this if after crop added.
+    uint32_t m_width;
+    uint32_t m_height;
 };
 
 uint32_t getMaxDecFrameBuffering(H264SPS * sps, uint32_t views);
