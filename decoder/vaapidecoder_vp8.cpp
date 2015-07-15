37595940ca5797fd3d3734713ed464a3ece93514
Xu Guangxin <Guangxin.Xu@intel.com>
2015-07-15 12:14:13 +0800

    vp8dec: add Gwenole Beauchesne to authors
    since previous patch is from https://github.com/01org/gstreamer-vaapi/commit/f2b0d02168e73feaae0023ccf864cea4805a0b88

--- decoder/vaapidecoder_vp8.cpp
+++ decoder/vaapidecoder_vp8.cpp
@@ -3,6 +3,7 @@
  *
  *  Copyright (C) 2013-2014 Intel Corporation
  *    Author: Zhao, Halley<halley.zhao@intel.com>
+ *    Author: Gwenole Beauchesne <gwenole.beauchesne@intel.com>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
