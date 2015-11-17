82b95351e6f0f3a99ed146f961f7d86e74ba8d92
U. Artie Eoff <ullysses.a.eoff@intel.com>
2015-11-17 14:19:30 -0800

    common/unittest: add some Factory unit tests
    These tests will also expose the SIGSEGV that was fixed by
    288028a98f4c.  That is, without 288028a98f4c the unittest
    executable will crash.
    
    Change-Id: Ie4a4f885ad18b957e89254e3944258d84fa96480
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ common/factory_unittest.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "factory.h"
+
+#define FACTORY_TEST(name) \
+    TEST(FactoryTest, name)
+
+class Base {
+public:
+    virtual ~Base() { }
+};
+
+class A : public Base { };
+class B : public Base { };
+class C : public Base { };
+
+// Attempt to expose what is commonly referred to as the
+// "static initialization order fiasco".
+// If the unittest program segfaults during pre-main static
+// initialization, then we've likely introduced an initialization
+// order bug.
+const bool rA = Factory<Base>::register_<A>("A");
+
+FACTORY_TEST(Register) {
+    EXPECT_TRUE(rA);
+    EXPECT_TRUE(Factory<Base>::register_<B>("B"));
+    EXPECT_TRUE(Factory<Base>::register_<B>("B2"));
+    EXPECT_TRUE(Factory<Base>::register_<C>("C"));
+    EXPECT_FALSE(Factory<Base>::register_<A>("A"));
+    EXPECT_FALSE(Factory<Base>::register_<B>("B"));
+    EXPECT_FALSE(Factory<Base>::register_<C>("C"));
+}
+
+FACTORY_TEST(Create) {
+    Base* a = Factory<Base>::create("A");
+    Base* z = Factory<Base>::create("Z");
+
+    EXPECT_TRUE(a != NULL);
+    EXPECT_TRUE(z == NULL);
+    EXPECT_TRUE(dynamic_cast<const A*>(a) != NULL);
+    EXPECT_TRUE(dynamic_cast<const B*>(a) == NULL);
+
+    delete a;
+}
