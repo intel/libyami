faffdb1bb9f586bb8d1e93b02907d91e8f926512
Zhong Li <zhong.li@intel.com>
2016-04-27 16:06:26 +0800

    vp8dec: add vp8 decoder
    1. Revert orignal vp8 decoder.
    2. Update it with the new vp8 parser
    3. Add some VP8 factory create tests
    Test that the VP8 decoder is registered correctly with
    the decoder factory.
    Test that the decoder can be created by the decoder factory
    from the YAMI_MIME_VP8 mime type.
    
    Change-Id: I27aef82477487c972cc9831589794400231543b3
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>
    Signed-off-by: Zhong Li <zhong.li@intel.com>
    Signed-off-by: Lin Hai <hai1.lin@intel.com>
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ decoder/vaapidecoder_vp8.h
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef vaapidecoder_vp8_h
+#define vaapidecoder_vp8_h
+
+#include "codecparsers/vp8_parser.h"
+#include "vaapidecoder_base.h"
+#include "vaapidecpicture.h"
+#include "va/va_dec_vp8.h"
+
+#if __PLATFORM_BYT__
+#define __PSB_CACHE_DRAIN_FOR_FIRST_FRAME__ 0
+#define __PSB_VP8_INTERFACE_WORK_AROUND__   1
+#else
+#define __PSB_CACHE_DRAIN_FOR_FIRST_FRAME__ 0
+#define __PSB_VP8_INTERFACE_WORK_AROUND__   0
+#endif
+
+namespace YamiMediaCodec{
+
+// function below taken from:
+// https://src.chromium.org/svn/trunk/src/third_party/cld/base/macros.h
+// The arraysize(arr) macro returns the # of elements in an array arr.
+// The expression is a compile-time constant, and therefore can be
+// used in defining new arrays, for example.  If you use arraysize on
+// a pointer by mistake, you will get a compile-time error.
+//
+// One caveat is that arraysize() doesn't accept any array of an
+// anonymous type or a type defined inside a function. This is
+// due to a limitation in C++'s template system.  The limitation might
+// eventually be removed, but it hasn't happened yet.
+
+// This template function declaration is used in defining arraysize.
+// Note that the function doesn't need an implementation, as we only
+// use its type.
+template <typename T, size_t N>
+char (&ArraySizeHelper(T (&array)[N]))[N];
+
+// That gcc wants both of these prototypes seems mysterious. VC, for
+// its part, can't decide which to use (another mystery). Matching of
+// template overloads: the final frontier.
+template <typename T, size_t N>
+char (&ArraySizeHelper(const T (&array)[N]))[N];
+
+#define arraysize(array) (sizeof(ArraySizeHelper(array)))
+
+enum {
+    VP8_EXTRA_SURFACE_NUMBER = 5,
+    VP8_MAX_PICTURE_COUNT = 5,  // gold_ref, alt_ref, last_ref, previous (m_currentPicture, optional), and the newly allocated one
+};
+
+class VaapiDecoderVP8:public VaapiDecoderBase {
+  public:
+    typedef SharedPtr<VaapiDecPicture> PicturePtr;
+    VaapiDecoderVP8();
+    virtual ~ VaapiDecoderVP8();
+    virtual Decode_Status start(VideoConfigBuffer * buffer);
+    virtual Decode_Status reset(VideoConfigBuffer * buffer);
+    virtual void stop(void);
+    virtual void flush(void);
+    virtual Decode_Status decode(VideoDecodeBuffer * buffer);
+
+  private:
+    bool allocNewPicture();
+    bool fillPictureParam(const PicturePtr& picture);
+    /* fill Quant matrix parameters */
+    bool ensureQuantMatrix(const PicturePtr& pic);
+    bool ensureProbabilityTable(const PicturePtr& pic);
+    bool fillSliceParam(VASliceParameterBufferVP8* sliceParam);
+    /* check the context reset senerios */
+    Decode_Status ensureContext();
+    /* decoding functions */
+    Decode_Status decodePicture();
+    void updateReferencePictures();
+
+  private:
+    friend class FactoryTest<IVideoDecoder, VaapiDecoderVP8>;
+    friend class VaapiDecoderVP8Test;
+
+    PicturePtr m_currentPicture;
+    PicturePtr m_lastPicture;
+    PicturePtr m_goldenRefPicture;
+    PicturePtr m_altRefPicture;
+
+    uint32_t m_hasContext:1;
+
+    // resolution of current frame, VP8 may change frame resolution starting with a key frame
+    uint32_t m_frameWidth;
+    uint32_t m_frameHeight;
+    const uint8_t *m_buffer;
+    uint32_t m_frameSize;
+    YamiParser::Vp8FrameHeader m_frameHdr;
+    YamiParser::Vp8Parser m_parser;
+    uint8_t m_yModeProbs[4];
+    uint8_t m_uvModeProbs[3];
+    uint32_t m_sizeChanged:1;
+
+#if __PSB_CACHE_DRAIN_FOR_FIRST_FRAME__
+    bool m_isFirstFrame;
+#endif
+
+    static const bool s_registered; // VaapiDecoderFactory registration result
+};
+}
+
+#endif
---
+++ decoder/vaapidecoder_vp8_unittest.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// primary header
+#include "vaapidecoder_vp8.h"
+
+// library headers
+#include "common/factory_unittest.h"
+
+namespace YamiMediaCodec {
+
+class VaapiDecoderVP8Test
+    : public FactoryTest<IVideoDecoder, VaapiDecoderVP8>
+{
+protected:
+    /* invoked by gtest before the test */
+    virtual void SetUp() {
+        return;
+    }
+
+    /* invoked by gtest after the test */
+    virtual void TearDown() {
+        return;
+    }
+};
+
+#define VAAPIDECODER_VP8_TEST(name) \
+    TEST_F(VaapiDecoderVP8Test, name)
+
+VAAPIDECODER_VP8_TEST(Factory) {
+    FactoryKeys mimeTypes;
+    mimeTypes.push_back(YAMI_MIME_VP8);
+    doFactoryTest(mimeTypes);
+}
+
+}
