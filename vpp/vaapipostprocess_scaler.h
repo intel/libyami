f6f1483a62dfd1424483ecb24ec9305ea96c2093
XuGuangxin <Guangxin.Xu@intel.com>
2016-06-28 14:14:10 +0800

    vpp: add denoise,sharpening and deinterlace bump api to 0.2.2
    we support denoise and sharpening level from 0 to 100
    support weave and bob for deinterlace
    
    Signed-off-by: Zhong Xiaoxia <xiaoxia.zhong@intel.com>
    Signed-off-by: Xu Guangxin <guangxin.xu@intel.com>

--- vpp/vaapipostprocess_scaler.h
+++ vpp/vaapipostprocess_scaler.h
@@ -18,16 +18,58 @@
 #define vaapipostprocess_scaler_h
 
 #include "vaapipostprocess_base.h"
+#include <vector>
+#include <set>
 
 namespace YamiMediaCodec{
 
 /* class for video scale and color space conversion */
 class VaapiPostProcessScaler : public VaapiPostProcessBase {
 public:
+    VaapiPostProcessScaler();
     virtual YamiStatus process(const SharedPtr<VideoFrame>& src,
                                const SharedPtr<VideoFrame>& dest);
 
+    virtual YamiStatus setParameters(VppParamType type, void* vppParam);
+
 private:
+    friend class FactoryTest<IVideoPostProcess, VaapiPostProcessScaler>;
+    friend class VaapiPostProcessScalerTest;
+
+    struct ProcParams {
+        int32_t level; //set by user
+        SharedPtr<VAProcFilterCap> caps; //query from va;
+        BufObjectPtr filter; //send to va;
+    };
+
+    struct DeinterlaceParams {
+        VppDeinterlaceMode mode; //set by user
+        std::set<VppDeinterlaceMode> supportedModes; //query from va
+        BufObjectPtr filter; //send to va;
+    };
+
+    bool mapToRange(float& value, float min, float max,
+        int32_t level, int32_t minLevel, int32_t maxLevel);
+
+    //map to driver's range
+    bool mapToRange(float& value, int32_t level, int32_t minLevel, int32_t maxLevel,
+        VAProcFilterType filterType, SharedPtr<VAProcFilterCap>& caps);
+
+    YamiStatus setParamToNone(ProcParams& params, int32_t none);
+
+    bool getFilters(std::vector<VABufferID>& filters);
+    YamiStatus createFilter(BufObjectPtr& filter, VAProcFilterType, float value);
+
+    YamiStatus setProcParams(ProcParams& params, int32_t level,
+        int32_t min, int32_t max, int32_t none, VAProcFilterType type);
+
+    YamiStatus setDeinterlaceParam(const VPPDeinterlaceParameters&);
+    YamiStatus createDeinterlaceFilter(const VPPDeinterlaceParameters&);
+
+    ProcParams m_denoise;
+    ProcParams m_sharpening;
+    DeinterlaceParams m_deinterlace;
+
     static const bool s_registered; // VaapiPostProcessFactory registration result
 };
 }
