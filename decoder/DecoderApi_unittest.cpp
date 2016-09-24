984d745b207a86a79e2fb40b50deb12a34c095b0
Xu Guangxin <guangxin.xu@intel.com>
2016-09-24 13:37:15 +0800

    unittest: add decoder api unit test
    All decoder should follow this api behaviours.
    This patch only add h264, we will add more in future
    
    This unit test will expose following unit test issues.
    Mainly because we did not return format change before actually
    allocate the surfaces.
    
        [----------] 1 test from VaapiDecoder/DecodeApiTest
        [ RUN      ] VaapiDecoder/DecodeApiTest.Format_Change/0
        libva info: VA-API version 0.39.3
        libva info: va_getDriverName() returns 0
        libva info: Trying to open /myopt/yami/vaapi/lib/dri/i965_drv_video.so
        libva info: Found init function __vaDriverInit_0_39
        libva info: va_openDriver() returns 0
        DecoderApi_unittest.cpp:335: Failure
        Value of: params->width
          Actual: 16
        Expected: m_width
        Which is: 0
        DecoderApi_unittest.cpp:336: Failure
        Value of: params->height
          Actual: 16
        Expected: m_height
        Which is: 0
        DecoderApi_unittest.cpp:335: Failure
        Value of: params->width
          Actual: 16
        Expected: m_width
        Which is: 8
        DecoderApi_unittest.cpp:336: Failure
        Value of: params->height
          Actual: 32
        Expected: m_height
        Which is: 8
        DecoderApi_unittest.cpp:360: Failure
        Value of: output->crop.width
          Actual: 8
        Expected: format.width
        Which is: 16
        DecoderApi_unittest.cpp:361: Failure
        Value of: output->crop.height
          Actual: 18
        Expected: format.height
        Which is: 16
        DecoderApi_unittest.cpp:414: Failure
        Value of: outFrames
          Actual: 1
        Expected: inFrames
        Which is: 3
        [  FAILED  ] VaapiDecoder/DecodeApiTest.Format_Change/0, where GetParam() = video/h264 (1 ms)

---
+++ decoder/DecoderApi_unittest.cpp
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+//
+// The unittest header must be included before va_x11.h (which might be included
+// indirectly).  The va_x11.h includes Xlib.h and X.h.  And the X headers
+// define 'Bool' and 'None' preprocessor types.  Gtest uses the same names
+// to define some struct placeholders.  Thus, this creates a compile conflict
+// if X defines them before gtest.  Hence, the include order requirement here
+// is the only fix for this right now.
+//
+// See bug filed on gtest at https://github.com/google/googletest/issues/371
+// for more details.
+//
+#include "common/factory_unittest.h"
+#include "common/common_def.h"
+#include "common/log.h"
+#include "decoder/FrameData.h"
+#include "vaapi/vaapidisplay.h"
+#include "vaapi/vaapisurfaceallocator.h"
+// primary header
+#include "VideoDecoderHost.h"
+
+// system headers
+#include <algorithm>
+#include <stdint.h>
+
+namespace YamiMediaCodec {
+
+static FrameData g_h264data[] = {
+    g_avc8x8I,
+    g_avc8x8P,
+    g_avc8x8B,
+    g_avc8x16,
+    g_avc16x16,
+    g_EOF,
+};
+
+class TestDecodeFrames {
+public:
+    typedef SharedPtr<TestDecodeFrames> Shared;
+
+    static Shared create(
+        const FrameData* data,
+        const char* mime)
+    {
+        Shared frames(new TestDecodeFrames(data, mime));
+        return frames;
+    }
+
+    const char* getMime() const
+    {
+        return m_mime;
+    }
+
+    bool getFrame(VideoDecodeBuffer& buffer, FrameInfo& info)
+    {
+        const FrameData& d = m_data[m_idx];
+        if (d.m_data != NULL) {
+            buffer.data = (uint8_t*)d.m_data;
+            buffer.size = d.m_size;
+            info = d.m_info;
+            m_idx++;
+            return true;
+        }
+        return false;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const TestDecodeFrames& t)
+    {
+        os << t.m_mime;
+        return os;
+    }
+
+private:
+    TestDecodeFrames(
+        const FrameData* data,
+        const char* mime)
+        : m_data(data)
+        , m_mime(mime)
+        , m_idx(0)
+    {
+    }
+
+    const FrameData* m_data;
+
+    const char* m_mime;
+    int m_idx;
+};
+
+class DecodeSurfaceAllocator : public VaapiSurfaceAllocator {
+public:
+    DecodeSurfaceAllocator(const DisplayPtr& display)
+        : VaapiSurfaceAllocator(display->getID())
+        , m_width(0)
+        , m_height(0)
+    {
+    }
+    void onFormatChange(const VideoFormatInfo* format)
+    {
+        m_width = format->surfaceWidth;
+        m_height = format->surfaceHeight;
+    }
+
+protected:
+    YamiStatus doAlloc(SurfaceAllocParams* params)
+    {
+        EXPECT_EQ(m_width, params->width);
+        EXPECT_EQ(m_height, params->height);
+        return VaapiSurfaceAllocator::doAlloc(params);
+    }
+    void doUnref()
+    {
+        delete this;
+    }
+
+private:
+    int32_t m_width;
+    int32_t m_height;
+};
+
+class DecodeApiTest
+    : public ::testing::Test,
+      public ::testing::WithParamInterface<TestDecodeFrames::Shared> {
+};
+
+bool checkOutput(SharedPtr<IVideoDecoder>& decoder, VideoFormatInfo& format)
+{
+    SharedPtr<VideoFrame> output(decoder->getOutput());
+    bool gotOutput = bool(output);
+    if (gotOutput) {
+        EXPECT_EQ(format.width, output->crop.width);
+        EXPECT_EQ(format.height, output->crop.height);
+    }
+    return gotOutput;
+}
+
+TEST_P(DecodeApiTest, Format_Change)
+{
+    NativeDisplay nativeDisplay;
+    memset(&nativeDisplay, 0, sizeof(nativeDisplay));
+    DisplayPtr display = VaapiDisplay::create(nativeDisplay);
+    DecodeSurfaceAllocator* allocator = new DecodeSurfaceAllocator(display);
+
+    SharedPtr<IVideoDecoder> decoder;
+    TestDecodeFrames frames = *GetParam();
+    decoder.reset(createVideoDecoder(frames.getMime()), releaseVideoDecoder);
+    ASSERT_TRUE(bool(decoder));
+
+    decoder->setAllocator(allocator);
+
+    VideoConfigBuffer config;
+    memset(&config, 0, sizeof(config));
+    ASSERT_EQ(YAMI_SUCCESS, decoder->start(&config));
+
+    VideoDecodeBuffer buffer;
+    FrameInfo info;
+    uint32_t inFrames = 0;
+    uint32_t outFrames = 0;
+    //keep previous resolution
+    VideoFormatInfo prevFormat;
+    memset(&prevFormat, 0, sizeof(prevFormat));
+
+    while (frames.getFrame(buffer, info)) {
+
+        YamiStatus status = decoder->decode(&buffer);
+        if (prevFormat.width != info.m_width
+            || prevFormat.width != info.m_width)
+        {
+            EXPECT_EQ(YAMI_DECODE_FORMAT_CHANGE, status);
+        }
+        while (checkOutput(decoder, prevFormat))
+            outFrames++;
+
+        if (status == YAMI_DECODE_FORMAT_CHANGE) {
+            //we need ouptut all previous frames
+            EXPECT_EQ(inFrames, outFrames);
+
+            //check format
+            const VideoFormatInfo* format = decoder->getFormatInfo();
+            ASSERT_TRUE(format);
+            EXPECT_EQ(info.m_width, format->width);
+            EXPECT_EQ(info.m_height, format->height);
+            allocator->onFormatChange(format);
+            prevFormat = *format;
+
+            //send buffer again
+            EXPECT_EQ(YAMI_SUCCESS, decoder->decode(&buffer));
+        } else {
+            EXPECT_EQ(YAMI_SUCCESS, status);
+        }
+        inFrames++;
+    }
+    //drain the decoder
+    EXPECT_EQ(YAMI_SUCCESS, decoder->decode(NULL));
+    while (checkOutput(decoder, prevFormat))
+        outFrames++;
+    EXPECT_TRUE(inFrames > 0);
+    EXPECT_EQ(inFrames, outFrames);
+}
+
+/** Teach Google Test how to print a TestDecodeFrames::Shared object */
+void PrintTo(const TestDecodeFrames::Shared& t, std::ostream* os)
+{
+    *os << *t;
+}
+
+INSTANTIATE_TEST_CASE_P(
+    VaapiDecoder, DecodeApiTest,
+    ::testing::Values(
+        TestDecodeFrames::create(g_h264data, YAMI_MIME_H264)));
+}
