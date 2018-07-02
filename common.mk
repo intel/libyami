9eb26e0fa8f0a63e153a7698973c0d5376a482b4
XuGuangxin <Guangxin.Xu@intel.com>
2018-07-02 15:00:21 +0800

    android: disable missing-braces error

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
-LOCAL_CFLAGS := -D__ENABLE_DEBUG__ -Wno-sign-compare -Wno-unused-parameter -O2
-LOCAL_CPPFLAGS := -D__ENABLE_DEBUG__ -Wno-sign-compare -Wno-unused-parameter -O2 -std=c++11
+LOCAL_CFLAGS := -D__ENABLE_DEBUG__ -Wno-sign-compare -Wno-unused-parameter -Wno-missing-braces -O2
+LOCAL_CPPFLAGS := -D__ENABLE_DEBUG__ -Wno-sign-compare -Wno-unused-parameter -Wno-missing-braces -O2 -std=c++11
 
 ENABLE-V4L2-OPS=true
